afb325b2e50bada1fb798d77109ff1c35b43b1d5
sangdo90 <sang10954@gmail.com>
2022-02-24 09:48:39 +0900

    temp

--- contracts/token/KIP37/KIP37TokenOwnable.sol
+++ contracts/token/KIP37/KIP37TokenOwnable.sol
@@ -4,10 +4,19 @@ import "./KIP37Token.sol";
 import "../../ownership/Ownable.sol";
 
 contract KIP37TokenOwnable is KIP37Token, Ownable {
-    constructor(string memory uri, address payable newOwner)
-        public
-        KIP37Token(uri)
-    {
+    // Contract name
+    string public name;
+    // Contract symbol
+    string public symbol;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory uri,
+        address payable newOwner
+    ) public KIP37Token(uri) {
         transferOwnership(newOwner);
+        name = _name;
+        symbol = _symbol;
     }
 }
